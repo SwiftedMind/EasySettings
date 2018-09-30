27ab0801f430e78457315c2345aca565a7ee83a3
Dennis Müller <d3mueller@me.com>
2018-09-30 21:22:01 +0200

    Initial Commit

---
+++ EasySettings/EasySettings.h
@@ -0,0 +1,19 @@
+//
+//  EasySettings.h
+//  EasySettings
+//
+//  Created by Dennis Müller on 30.09.18.
+//  Copyright © 2018 Dennis Müller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EasySettings.
+FOUNDATION_EXPORT double EasySettingsVersionNumber;
+
+//! Project version string for EasySettings.
+FOUNDATION_EXPORT const unsigned char EasySettingsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EasySettings/PublicHeader.h>
+
+
